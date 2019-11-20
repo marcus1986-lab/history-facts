69cf62d1f6e940cec7918324da62c63f025d4fad
marcus1986-lab <57982396+marcus1986-lab@users.noreply.github.com>
2019-11-20 10:39:04 +0100

    Add files via upload

---
+++ languageStrings.js
@@ -0,0 +1,22 @@
+var languageStrings = {
+    "en": {
+        "translation": {
+            "FACTS": [
+                "In 1926, Arctic explorer Peter Freuchen was trapped under an avalanche while on an expedition. He escaped from death by fashioning a shiv out of his own feces and amputating his foot.",
+                "In ancient Egypt, servants were smeared with honey to attract flies away from the pharaoh..",
+                "The first fax was sent while people were still traveling the Oregon Trail.",
+                "Before Abraham Lincoln became a politician, he was a champion wrestler. With more than 300 bouts under his belt, Lincoln only lost one match in his career and was inducted into the National Wrestling Hall Of Fame in 1992.",
+                "In the Victorian era, it was popular for people to photograph relatives after they had died, often placing them in lifelike poses. ",
+                "In 1923, jockey Frank Hayes won a race at Belmont Park in New York despite being dead. He suffered a heart attack mid-race, but his body stayed in the saddle until his horse crossed the line for a 20-1 outsider victory.",
+                "All British tanks since 1945 have included equipment to make tea."
+            ],
+            "SKILL_NAME" : "History Facts",
+            "GET_FACT_MESSAGE" : "Here's your fact: ",
+            "HELP_MESSAGE" : "You can say tell me a history fact, or, you can say exit... What can I help you with?",
+            "HELP_REPROMPT" : "What can I help you with?",
+            "STOP_MESSAGE" : "Goodbye!"
+        }
+    }
+};
+
+module.exports = languageStrings;
